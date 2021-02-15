7f3671d89becefcd4e5a074c26e3c458aa73b060
zhangsan <zanghu20080701@126.com>
2021-02-15 16:36:13 +0800

    修改描述

--- nn/math_utils.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <math.h>
-
-float rand_uniform(float min, float max);
--- nn/vcheck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-valgrind --tool=memcheck --track-fds=yes --leak-check=full --show-leak-kinds=all --undef-value-errors=yes --track-origins=yes ./Test
-#valgrind --tool=memcheck --track-fds=yes --leak-check=full --show-leak-kinds=all --undef-value-errors=yes --track-origins=yes --log-file=vcheck.log ./Test
-#valgrind --tool=memcheck --track-fds=yes --leak-check=full --show-leak-kinds=all --undef-value-errors=yes --log-file=vcheck.log ./Test
-#valgrind --tool=memcheck --track-fds=yes --leak-check=full --show-leak-kinds=all --undef-value-errors=yes --log-file=vcheck.log --xml-file=vcheck.xml --xml=yes ./Test
